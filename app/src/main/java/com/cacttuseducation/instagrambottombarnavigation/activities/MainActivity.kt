4016e5727a87747aae7030aba59413f974383357
Shqipdon Mehmeti <keds30671@KEDS.kv>
2022-12-13 18:49:57 +0100

    managing instagrm bottom navigation

--- app/src/main/java/com/cacttuseducation/instagrambottombarnavigation/activities/MainActivity.kt
+++ app/src/main/java/com/cacttuseducation/instagrambottombarnavigation/activities/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.cacttuseducation.instagrambottombarnavigation.R
 import com.cacttuseducation.instagrambottombarnavigation.databinding.ActivityMainBinding
+import com.cacttuseducation.instagrambottombarnavigation.fragments.ExploreFragment
+import com.cacttuseducation.instagrambottombarnavigation.fragments.HomeFragment
+import com.cacttuseducation.instagrambottombarnavigation.fragments.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -12,7 +15,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        onBottomNavigationClickHandler()
+    }
 
+    private fun onBottomNavigationClickHandler() {
+        val homeFragment = HomeFragment()
+        val exploreFragment = ExploreFragment()
+        val profileFragment = ProfileFragment()
+
+        binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.homeMenuId -> setCurrentFragment(homeFragment)
+                R.id.exploreMenuId -> setCurrentFragment(exploreFragment)
+                R.id.profileMenuId -> setCurrentFragment(profileFragment)
+            }
+            true
+
+        }
     }
 
     private fun setCurrentFragment(currentFragment : Fragment) {
@@ -22,4 +41,6 @@ class MainActivity : AppCompatActivity() {
             commit()
         }
     }
+
+
 }
